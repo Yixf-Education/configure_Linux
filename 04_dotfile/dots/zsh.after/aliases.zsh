80f4df8bece1445a53cd26b77d9c0dc51a690e9d
Yixf(Yi Xianfu) <yixfgit@gmail.com>
2017-04-05 17:06:49 +0800

    Add dots for myself

---
+++ 04_dotfile/dots/zsh.after/aliases.zsh
@@ -0,0 +1,37 @@
+### Yixf's alias
+
+# Common
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias wget='wget -c'
+alias yg='you-get'
+alias yd='proxychains youtube-dl'
+
+# Apt
+alias aa='sudo apt'
+alias ai='sudo apt install'
+alias au='sudo apt update'
+alias ag='sudo apt upgrade'
+alias aug='sudo apt update; sudo apt upgrade'
+alias ap='sudo apt purge'
+alias ac='sudo apt clean'
+alias am='sudo apt remove'
+alias aac='sudo apt autoclean'
+alias aar='sudo apt autoremove'
+alias adu='sudo apt dselect-upgrade'
+alias aud='sudo apt update; sudo apt dist-upgrade'
+alias di='sudo dpkg -i'
+
+# LaTeX
+alias mk="latexmk -pdf -pvc -xelatex"
+alias mc="latexmk -c"
+
+# Git
+alias glz='git zlog'
+
+# Pygments
+#alias cat='pygmentize -O style=tango -f console256 -g'
+#alias cat='pygmentize -O style=fruity -f console256 -g'
+
---
+++ 04_dotfile/dots/zsh.after/zshrc.zsh
@@ -0,0 +1,2 @@
+# added by Miniconda3 4.3.11 installer
+export PATH="/home/yixf/Miniconda3/bin:$PATH"
