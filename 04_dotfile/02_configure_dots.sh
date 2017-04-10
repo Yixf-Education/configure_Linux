892aab726ce2e83008ab98be988583720f11ee21
Yixf(Yi Xianfu) <yixfgit@gmail.com>
2017-04-10 11:59:54 +0800

    Modify dotfile scripts

--- 04_dotfile/02_configure_dots.sh
+++ 04_dotfile/02_configure_dots.sh
@@ -7,6 +7,8 @@ cp $dir_dot/zsh.after/*.zsh $HOME/.zsh.after
 cp $dit_dot/vim_settings/*.vim $HOME/.yadr/vim/settings
 cp $dir_dot/UltiSnips $HOME/.yadr/vim/
 cp $dir_dot/vim_vundles/*.vundle $HOME/.yadr/vim/vundles
+sed -i '/vim-snipmate/ s/^/"/' $HOME/.yadr/vim/vundles/languages.vundle
+sed -i '/vim-snippets/ s/^/"/' $HOME/.yadr/vim/vundles/languages.vundle
 sed -i '/^runtime ruby.vundle/ s/^/"/' $HOME/.yadr/vim/vundles.vim
 sed -i '/runtime vim-improvements.vundle/ a\runtime vim-yixf.vundle' $HOME/.yadr/vim/vundles.vim
 cat $dir_dot/vimrc >> $HOME/.yadr/vimrc
---
+++ 04_dotfile/dots/vim_settings/ultisnips.vim
@@ -0,0 +1,7 @@
+" Trigger configuration. Do not use <tab> if you use https://github.com/Valloric/YouCompleteMe.
+let g:UltiSnipsExpandTrigger="<c-b>"
+let g:UltiSnipsJumpForwardTrigger="<c-j>"
+let g:UltiSnipsJumpBackwardTrigger="<c-k>"
+"let g:UltiSnipsListSnippets="<c-h>"
+" If you want :UltiSnipsEdit to split your window.
+let g:UltiSnipsEditSplit="vertical"
