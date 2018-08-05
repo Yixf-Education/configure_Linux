ac614a7e30623fd21c94c9309f96373c0cd23dc0
Yixf-Education <yixfgit@gmail.com>
2018-08-05 11:18:39 +0800

    Modify conda configure

--- 03_bio/02_set_up_channels.sh
+++ 03_bio/02_set_up_channels.sh
@@ -1,5 +1,13 @@
-conda config --add channels conda-forge
-conda config --add channels defaults
-conda config --add channels r
-conda config --add channels bioconda
+#conda config --add channels defaults
+#conda config --add channels conda-forge
+#conda config --add channels r
+#conda config --add channels bioconda
+
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/r/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/bioconda/
+conda config --set show_channel_urls yes
+
 exit $?
